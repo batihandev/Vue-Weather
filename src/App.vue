452e7a30e511b04d0335f6f81896d068750374e8
batihanozio <batihandev@gmail.com>
2023-02-28 09:13:31 +0300

    weather app vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex min-h-screen flex-col bg-weather-primary font-Roboto">
+    <SiteNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import SiteNavigation from './components/SiteNavigation.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="sticky top-0 bg-weather-primary shadow-lg">
+    <nav class="container flex flex-col items-center gap-4 py-6 text-white sm:flex-row">
+      <RouterLink :to="{ name: 'home' }">
+        <div class="flex items-center gap-3">
+          <i class="fa-solid fa-sun text-2xl"></i>
+          <p class="text-2xl">The Local Weather</p>
+        </div>
+      </RouterLink>
+
+      <div class="flex flex-1 justify-end gap-3">
+        <i
+          class="fa-solid fa-circle-info cursor-pointer text-xl duration-150 hover:text-weather-secondary"
+        ></i>
+        <i
+          class="fa-solid fa-plus cursor-pointer text-xl duration-150 hover:text-weather-secondary"
+        ></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
